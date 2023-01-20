3b6695c29448ecd4ff0a626bfe87604e14bf6c73
BaekSeungHo <vudrkd1580@naver.com>
2023-01-20 11:06:38 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -29,21 +29,29 @@ window.addEventListener('scroll', (e) => {
         $('#model').html('Model Y');
         $("#application").show();
         $('.order').show();
-      }else if((position1 - position2)==-969){
+      }else if(position1 - position2<0&&(position1 - position2)>=-969){
         $('#model').html('Model 3');
         $("#application").show();
         $('.order').show();
-      }else if((position1 - position2)==-1938){
+      }else if((position1 - position2)<=-969&&(position1 - position2)>=-1938){
         $('#model').html('Model X');
         $("#application").show();
         $('.order').show();
-      }else if((position1 - position2)==-2907){
+      }else if((position1 - position2)<=-1938&&(position1 - position2)>=-2907){
         $('#model').html('Model S');
-        $("#application").show();
+        $("#application").html('시승 신청');
+        $('#application').css('border-bottom','1px solid#333');
+        $('.detail').css('background-color','rgba(255, 255, 255, 0.6)');
+        $('.detail').css('color','#333');
+        $('.order-box').css('bottom','125px')
         $('.order').show();
-      }else if((position1 - position2)==-3876){
+      }else if((position1 - position2)<=-2907&&(position1 - position2)>=-3876){
         $('#model').html('태양광과 Powerwall');
-        $("#application").hide();
+        $("#application").html('모든 전력을 공급');
+        $('#application').css('border-bottom','none');
+        $('.detail').css('background-color','rgba(0, 0, 0, .6)');
+        $('.detail').css('color','#fff');
+        $('.order-box').css('bottom','170px')
         $('.order').hide();
       }
 
